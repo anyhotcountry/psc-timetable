2270519376479223cd1f114aed8ae90d8efd4541
anyhotcountry <anyhotcountry@users.noreply.github.com>
2025-10-20 17:59:54 +0000

    Fix end time of lessons. Include data for two weeks instead of current week only

--- app.py
+++ app.py
@@ -28,20 +28,33 @@ def login(session):
     response.raise_for_status()
     return session
 
-def get_timetable(session):
-    c = Calendar()
-    timetable_url = os.getenv("TIMETABLE_URL")
-
+def get_timetable(session, timetable_url, events = None):
+    base_url = os.getenv("BASE_URL")
+    is_current_week = events is None
+    events = [] if events is None else events
     response = session.get(timetable_url)
     soup = BeautifulSoup(response.text, "html.parser")
     periods = soup.find_all("td", {"class": "cell-item", "data-start": True, "data-end": True})
     for x in periods:
         e = Event()
         e.name = x.find("span", {"class": "title"}).get_text()
-        start_time = datetime.strptime(x.get("data-start"), DATE_FORMAT).replace(tzinfo=tz.gettz('Europe/London'))
-        end_time = datetime.strptime(x.get("data-end"), DATE_FORMAT).replace(tzinfo=tz.gettz('Europe/London'))
+        end = x.find("span", {"class": "times"}).get_text().split(" - ")[-1]
+        data_start = x.get("data-start")
+        start_time = datetime.strptime(data_start, DATE_FORMAT).replace(tzinfo=tz.gettz('Europe/London'))
+        end_time = datetime.strptime(f"{data_start.split(' ')[0]} {end}:00", DATE_FORMAT).replace(tzinfo=tz.gettz('Europe/London'))
         e.begin = start_time.astimezone(tz.tzutc()).strftime(DATE_FORMAT)
         e.end = end_time.astimezone(tz.tzutc()).strftime(DATE_FORMAT)
+        events.append(e)
+    next_link = soup.find("a", {"class": "next"})
+    next_url = requests.compat.urljoin(base_url, next_link.get("href")) if next_link and is_current_week else None
+    if next_url:
+        return get_timetable(session, next_url, events)
+    return events
+
+def build_calendar(session):
+    c = Calendar()
+    events = get_timetable(session, os.getenv("TIMETABLE_URL"), [])
+    for e in events:
         c.events.add(e)
     return c.serialize()
 
@@ -53,7 +66,7 @@ def home():
 def timetable():
     session = requests.Session()
     login(session)
-    calendar_data = get_timetable(session)
+    calendar_data = build_calendar(session)
     return calendar_data, {'Content-Type': 'text/calendar'}
 
 if __name__ == "__main__":
